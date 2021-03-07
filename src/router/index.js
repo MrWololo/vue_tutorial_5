8716d0e744d9a560b34e63434f6a377be0ac0e9d
MrWololo <martin_pedrosa@hotmail.com>
2021-03-07 08:47:44 -0300

    final

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import About from "../views/About.vue";
+import Jobs from "../views/Jobs/Jobs.vue";
+import JobDetails from "../views/Jobs/JobDetails.vue";
+import NotFound from "../views/NotFound.vue";
 
 const routes = [
   {
@@ -13,6 +16,27 @@ const routes = [
     name: "About",
     component: About,
   },
+  {
+    path: "/jobs",
+    name: "Jobs",
+    component: Jobs,
+  },
+  {
+    path: "/jobs/:id",
+    name: "JobDetails",
+    component: JobDetails,
+    props: true,
+  },
+
+  {
+    path: "/all-jobs",
+    redirect: "/Jobs",
+  },
+  {
+    path: "/:catchAll(.*)",
+    name: 'NotFound',
+    component: NotFound,
+  }
 ];
 
 const router = createRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>HomePage</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis sit
+      enim architecto inventore nobis tempora quod! Unde, error incidunt animi,
+      veritatis est, quas labore facere dolores ut quo et doloribus?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste nobis
+      beatae, quia deleniti alias animi fuga dicta explicabo accusantium. Culpa
+      eaque fuga perferendis reprehenderit! Ipsa dolor impedit error itaque
+      ratione.
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+  },
+};
 </script>
